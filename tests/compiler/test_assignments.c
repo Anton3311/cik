7a595ef2bf56c6339611389e45085562d13c0e2e
Anton3311 <antonplakhotia@gmail.com>
2026-07-21 11:09:03 +0300

    tests: Updated `test_assignments.c` to include all of the supported ones

--- tests/compiler/test_assignments.c
+++ tests/compiler/test_assignments.c
@@ -10,5 +10,17 @@ int main(int argc, char* argv[]) {
 	int b = 10;
 	b -= 4;
 	assert(b == 6);
+
+	int c = 10;
+	c *= 4;
+	assert(c == 40);
+
+	int d = 10;
+	d /= 4;
+	assert(d == 2);
+
+	int e = 47;
+	e %= 8;
+	assert(e == 7);
 	return 0;
 }

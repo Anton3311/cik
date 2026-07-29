bb7dcda1d875b6579469d14f9bdaae8a603be9a8
Anton3311 <antonplakhotia@gmail.com>
2026-07-29 17:57:15 +0300

    code_gen: Implemented `SymbolMap` growing

--- code_gen/src/code_gen/code_gen.c
+++ code_gen/src/code_gen/code_gen.c
@@ -4,33 +4,91 @@
 // SymbolMap
 //
 
-static const size_t SYMBOL_MAP_INITIAL_CAPACITY = 32;
+static const size_t SYMBOL_MAP_INITIAL_CAPACITY = 8;
 
-inline size_t _symbol_look_up_by_name_and_linkage(const SymbolMap* map, String name) {
+// Returns `SIZE_MAX` when the key already exists
+inline size_t _find_symbol_insert_index(const SymbolKey* keys, SymbolKey key, size_t capacity) {
+	profile_scope_start(__func__);
+	size_t hash = hash_string(key.name);
+
+	for (size_t i = 0; i < capacity; i += 1) {
+		size_t index = (hash + i) % capacity;
+
+		if (keys[index].name.v == NULL) {
+			profile_scope_end();
+			return index;
+		} else if (keys[index].linkage == key.linkage && str_equal(keys[index].name, key.name)) {
+			profile_scope_end();
+			return SIZE_MAX;
+		}
+	}
+
+	unreachable();
 	return SIZE_MAX;
 }
 
 static void _symbol_map_grow(SymbolMap* map) {
-	
+	profile_scope_start(__func__);
+	size_t new_capacity = max(SYMBOL_MAP_INITIAL_CAPACITY, map->capacity * 2);
+
+	// Reallocate the array
+
+	Symbol* new_symbols = allocator_alloc_array(map->allocator, Symbol, new_capacity);
+
+	if (map->symbols) {
+		memcpy(new_symbols, map->symbols, map->count * sizeof(*new_symbols));
+		allocator_release(map->allocator, map->symbols);
+	}
+
+	// Free the hash map, since it will be rebuilt from scratch
+	if (map->keys) {
+		allocator_release(map->allocator, map->keys);
+	}
+
+	if (map->values) {
+		allocator_release(map->allocator, map->values);
+	}
+
+	// Reallocate the hash map
+
+	size_t new_map_entry_count = new_capacity * 100 / 80;
+
+	SymbolKey* new_keys = allocator_alloc_array(map->allocator, SymbolKey, new_map_entry_count);
+	SymbolId* new_values = allocator_alloc_array(map->allocator, SymbolId, new_map_entry_count);
+
+	memset(new_keys, 0, sizeof(*new_keys) * new_map_entry_count);
+	memset(new_values, 0xff, sizeof(*new_values) * new_map_entry_count);
+
+	size_t symbol_count = map->count;
+	for (size_t i = 0; i < symbol_count; i += 1) {
+		SymbolKey key = symbol_key_from_symbol(&new_symbols[i]);
+		size_t insert_index = _find_symbol_insert_index(new_keys, key, new_map_entry_count);
+
+		assert(insert_index != SIZE_MAX);
+
+		new_values[insert_index] = (SymbolId)i;
+		new_keys[insert_index] = key;
+	}
+
+	map->symbols = new_symbols;
+	map->keys = new_keys;
+	map->values = new_values;
+	map->capacity = new_capacity;
+	map->map_capacity = new_map_entry_count;
+
+	profile_scope_end();
 }
 
 void symbol_map_init(SymbolMap* map, Allocator allocator) {
-	profile_scope_start(__func__);
-
 	map->allocator = allocator;
 
 	map->count = 0;
-	map->capacity = SYMBOL_MAP_INITIAL_CAPACITY;
-	map->symbols = allocator_alloc_array(allocator, Symbol, SYMBOL_MAP_INITIAL_CAPACITY);
+	map->capacity = 0;
+	map->symbols = NULL;
 
-	size_t map_entry_count = map->capacity * 100 / 80;
-	map->map_capacity = map_entry_count;
-	map->keys = allocator_alloc_array(allocator, SymbolKey, map_entry_count);
-	map->values = allocator_alloc_array(allocator, uint32_t, map_entry_count);
-
-	memset(map->keys, 0, sizeof(*map->keys) * map_entry_count);
-
-	profile_scope_end();
+	map->map_capacity = 0;
+	map->keys = NULL;
+	map->values = NULL;
 }
 
 void symbol_map_release(SymbolMap* map) {
@@ -52,44 +110,29 @@ void symbol_map_release(SymbolMap* map) {
 	profile_scope_end();
 }
 
-inline bool _symbol_key_equal(const SymbolKey a, const SymbolKey b) {
-	if (a.linkage != b.linkage) {
-		return false;
-	}
-
-	return str_equal(a.name, b.name);
-}
-
 SymbolId symbol_map_insert(SymbolMap* map, const Symbol* symbol) {
 	profile_scope_start(__func__);
-	assert(map->count < map->capacity);
 
-	size_t hash = hash_string(symbol->name);
-
-	for (size_t i = 0; i < map->map_capacity; i += 1) {
-		size_t index = (hash + i) % map->map_capacity;
-
-		if (map->keys[index].name.v == NULL) {
-			SymbolId id = (SymbolId)map->count;
-
-			map->symbols[id] = *symbol;
-			map->values[index] = id;
-			map->keys[index] = (SymbolKey) {
-				.name = symbol->name,
-				.linkage = symbol->linkage,
-			};
-
-			map->count += 1;
-			profile_scope_end();
-			return id;
-		} else if (_symbol_key_equal(map->keys[index], symbol_key_from_symbol(symbol))) {
-			profile_scope_end();
-			return SYMBOL_ID_INVALID;
-		}
+	if (map->count == map->capacity) {
+		_symbol_map_grow(map);
 	}
 
+	SymbolKey key = symbol_key_from_symbol(symbol);
+	size_t insert_index = _find_symbol_insert_index(map->keys, key, map->map_capacity);
+
+	if (insert_index == SIZE_MAX) {
+		profile_scope_end();
+		return SYMBOL_ID_INVALID;
+	}
+
+	SymbolId id = (SymbolId)map->count;
+
+	map->symbols[id] = *symbol;
+	map->values[insert_index] = id;
+	map->keys[insert_index] = key;
+	map->count += 1;
 	profile_scope_end();
-	return SYMBOL_ID_INVALID;
+	return id;
 }
 
 SymbolId symbol_map_insert_dynamically_linked_impl(SymbolMap* map, String name, void* impl) {

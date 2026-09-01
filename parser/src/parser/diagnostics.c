9ca7a516b920a9dcfede8931d72cfd45e4cb5785
Anton3311 <antonplakhotia@gmail.com>
2026-09-01 09:34:29 +0300

    parser/diagnostics: Refactored error printing to reduce indentation

--- parser/src/parser/diagnostics.c
+++ parser/src/parser/diagnostics.c
@@ -6,78 +6,89 @@ void _print_indent(size_t count) {
 	}
 }
 
-void _diagnostics_print_entry(const Diagnostics* diagnostics, const DiagnosticsEntry* entry, size_t indent) {
-	while (entry != NULL) {
-		// TODO: Handle multiple highlight ranges
-		assert(entry->highlighted_range_count == 1);
+void _diagnostics_print_entry(const Diagnostics* diagnostics,
+		const DiagnosticsEntry* entry,
+		size_t indent);
 
-		const SourceFile* source_file = entry->source_file;
-		assert(source_file);
-		const LineInfo* line_info = &source_file->line_info;
+void _diagnostics_print_entry_list(const Diagnostics* diagnostics,
+		const DiagnosticsEntry* entry,
+		size_t indent) {
 
-		uint32_t start_line = entry->start_line;
-		if (start_line > 0) {
-			start_line -= 1;
-		}
-
-		size_t highlight_index = 0;
-
-		printf("\n");
-
-		uint32_t end_line = min(entry->end_line + 1, line_info->line_count - 1);
-		for (uint32_t line = start_line; line <= end_line; line += 1) {
-			String source_line = line_info_get_line_string(line_info, source_file->source_code, line);
-
-			SourceRange line_range = line_info_get_line_range(line_info, line);
-
-			// Remove line ending from the line string
-			switch (str_get_line_ending_type(source_line)) {
-			case LINE_ENDING_NONE:
-				break;
-			case LINE_ENDING_LF:
-				line_range.end -= 1;
-				source_line.length -= 1;
-				break;
-			case LINE_ENDING_CRLF:
-				line_range.end -= 2;
-				source_line.length -= 2;
-				break;
-			}
-
-			SourceRange highlight_range = entry->highlighted_ranges[highlight_index];
-			size_t highlight_start = min(max(highlight_range.start, line_range.start), line_range.end);
-			size_t highlight_end = min(max(highlight_range.end, line_range.start), line_range.end);
-
-			// Normalize to range [0; source_line.length]
-			highlight_start -= line_range.start;
-			highlight_end -= line_range.start;
-
-			String before_highlight = sub_str(source_line, 0, highlight_start);
-			String highlighted_sub_str = sub_str(source_line, highlight_start, highlight_end - highlight_start);
-			String after_highlight = sub_str(source_line, highlight_end, source_line.length - highlight_end);
-
-			_print_indent(indent);
-			printf("\t%u: %.*s\033[1;31m%.*s\033[0m%.*s\n",
-					line + 1,
-					STR_FMT(before_highlight),
-					STR_FMT(highlighted_sub_str),
-					STR_FMT(after_highlight));
-		}
-
-		_print_indent(indent);
-		printf("%.*s: %.*s\n", STR_FMT(source_file->path), STR_FMT(entry->message));
-
-		if (entry->first_child) {
-			_diagnostics_print_entry(diagnostics, entry->first_child, indent + 1);
-		}
-
+	while (entry) {
+		_diagnostics_print_entry(diagnostics, entry, 0);
 		entry = entry->next;
 	}
 }
 
+void _diagnostics_print_entry(const Diagnostics* diagnostics,
+		const DiagnosticsEntry* entry,
+		size_t indent) {
+	// TODO: Handle multiple highlight ranges
+	assert(entry->highlighted_range_count == 1);
+
+	const SourceFile* source_file = entry->source_file;
+	assert(source_file);
+	const LineInfo* line_info = &source_file->line_info;
+
+	uint32_t start_line = entry->start_line;
+	if (start_line > 0) {
+		start_line -= 1;
+	}
+
+	size_t highlight_index = 0;
+
+	printf("\n");
+
+	uint32_t end_line = min(entry->end_line + 1, line_info->line_count - 1);
+	for (uint32_t line = start_line; line <= end_line; line += 1) {
+		String source_line = line_info_get_line_string(line_info, source_file->source_code, line);
+
+		SourceRange line_range = line_info_get_line_range(line_info, line);
+
+		// Remove line ending from the line string
+		switch (str_get_line_ending_type(source_line)) {
+		case LINE_ENDING_NONE:
+			break;
+		case LINE_ENDING_LF:
+			line_range.end -= 1;
+			source_line.length -= 1;
+			break;
+		case LINE_ENDING_CRLF:
+			line_range.end -= 2;
+			source_line.length -= 2;
+			break;
+		}
+
+		SourceRange highlight_range = entry->highlighted_ranges[highlight_index];
+		size_t highlight_start = min(max(highlight_range.start, line_range.start), line_range.end);
+		size_t highlight_end = min(max(highlight_range.end, line_range.start), line_range.end);
+
+		// Normalize to range [0; source_line.length]
+		highlight_start -= line_range.start;
+		highlight_end -= line_range.start;
+
+		String before_highlight = sub_str(source_line, 0, highlight_start);
+		String highlighted_sub_str = sub_str(source_line, highlight_start, highlight_end - highlight_start);
+		String after_highlight = sub_str(source_line, highlight_end, source_line.length - highlight_end);
+
+		_print_indent(indent);
+		printf("\t%u: %.*s\033[1;31m%.*s\033[0m%.*s\n",
+				line + 1,
+				STR_FMT(before_highlight),
+				STR_FMT(highlighted_sub_str),
+				STR_FMT(after_highlight));
+	}
+
+	_print_indent(indent);
+	printf("%.*s: %.*s\n", STR_FMT(source_file->path), STR_FMT(entry->message));
+
+	if (entry->first_child) {
+		_diagnostics_print_entry(diagnostics, entry->first_child, indent + 1);
+	}
+}
+
 void diagnostics_print(const Diagnostics* diagnostics) {
-	DiagnosticsEntry* entry = diagnostics->first;
-	_diagnostics_print_entry(diagnostics, entry, 0);
+	_diagnostics_print_entry_list(diagnostics, diagnostics->first, 0);
 }
 
 DiagnosticsEntry* diagnostics_report_error(Diagnostics* diagnostics,
